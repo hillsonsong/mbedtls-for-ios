b518baaf3c1c3deb66260aa9d678b69a57a92a7d
annidyfeng <annidy@gmail.com>
2015-10-27 18:20:30 +0800

    update to mbedtls 2.1.2

--- build_ios.sh
+++ build_ios.sh
@@ -22,23 +22,23 @@
 ###########################################################################
 #  Change values here
 #
-VERSION="2.0.0"
+VERSION="2.1.2"
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`
 #
 ###########################################################################
 #
 # Don't change anything here
 CURRENTPATH=`pwd`
-ARCHS="i386 x86_64 armv7 armv7s arm64"
+ARCHS="i386 armv7 arm64"
 DEVELOPER=`xcode-select -print-path`
 
 ##########
 set -e
-if [ ! -e mbedtls-${VERSION}-gpl.tgz ]; then
-echo "Downloading mbedtls-${VERSION}-gpl.tgz"
-curl -O https://tls.mbed.org/download/start/mbedtls-${VERSION}-gpl.tgz
+if [ ! -e mbedtls-${VERSION}-apache.tgz ]; then
+	echo "Downloading mbedtls-${VERSION}-apache.tgz"
+	curl -O https://tls.mbed.org/download/mbedtls-${VERSION}-apache.tgz
 else
-echo "Using mbedtls-${VERSION}-gpl.tgz"
+	echo "Using mbedtls-${VERSION}-apache.tgz"
 fi
 
 mkdir -p bin
@@ -47,49 +47,49 @@ mkdir -p src
 
 for ARCH in ${ARCHS}
 do
-if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
-then
-PLATFORM="iPhoneSimulator"
-else
-PLATFORM="iPhoneOS"
-fi
+	if [[ "${ARCH}" == "i386" ]];
+	then
+		PLATFORM="iPhoneSimulator"
+	else
+		PLATFORM="iPhoneOS"
+	fi
 
-tar zxvf mbedtls-${VERSION}-gpl.tgz -C src
-cd src/mbedtls-${VERSION}/library
+	tar zxvf mbedtls-${VERSION}-apache.tgz -C src
+	cd src/mbedtls-${VERSION}/library
 
-echo "Building mbedtls for ${PLATFORM} ${SDKVERSION} ${ARCH}"
+	echo "Building mbedtls for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 
-echo "Patching Makefile..."
-sed -i.bak '4d' ${CURRENTPATH}/src/mbedtls-${VERSION}/library/Makefile
+	echo "Patching Makefile..."
+	sed -i.bak '4d' ${CURRENTPATH}/src/mbedtls-${VERSION}/library/Makefile
 
-echo "Please stand by..."
+	echo "Please stand by..."
 
-export DEVROOT="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
-export BUILD_TOOLS="${DEVELOPER}"
-export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
-#export CC=${BUILD_TOOLS}/usr/bin/gcc
-#export LD=${BUILD_TOOLS}/usr/bin/ld
-#export CPP=${BUILD_TOOLS}/usr/bin/cpp
-#export CXX=${BUILD_TOOLS}/usr/bin/g++
-#export AR=${DEVROOT}/usr/bin/ar
-#export AS=${DEVROOT}/usr/bin/as
-#export NM=${DEVROOT}/usr/bin/nm
-#export CXXCPP=${BUILD_TOOLS}/usr/bin/cpp
-#export RANLIB=${BUILD_TOOLS}/usr/bin/ranlib
-export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT}"
-export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/src/mbedtls-${VERSION}/include"
+	export DEVROOT="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
+	export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+	export BUILD_TOOLS="${DEVELOPER}"
+	export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
+	#export CC=${BUILD_TOOLS}/usr/bin/gcc
+	#export LD=${BUILD_TOOLS}/usr/bin/ld
+	#export CPP=${BUILD_TOOLS}/usr/bin/cpp
+	#export CXX=${BUILD_TOOLS}/usr/bin/g++
+	#export AR=${DEVROOT}/usr/bin/ar
+	#export AS=${DEVROOT}/usr/bin/as
+	#export NM=${DEVROOT}/usr/bin/nm
+	#export CXXCPP=${BUILD_TOOLS}/usr/bin/cpp
+	#export RANLIB=${BUILD_TOOLS}/usr/bin/ranlib
+	export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT}"
+	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/src/mbedtls-${VERSION}/include"
 
-make
+	make
 
-cp libmbedtls.a ${CURRENTPATH}/bin/libmbedtls-${ARCH}.a
-cp -R ${CURRENTPATH}/src/mbedtls-${VERSION}/include ${CURRENTPATH}
-cp ${CURRENTPATH}/src/mbedtls-${VERSION}/LICENSE ${CURRENTPATH}/include/mbedtls/LICENSE
-cd ${CURRENTPATH}
-rm -rf src/mbedtls-${VERSION}
+	cp libmbedtls.a ${CURRENTPATH}/bin/libmbedtls-${ARCH}.a
+	cp -R ${CURRENTPATH}/src/mbedtls-${VERSION}/include ${CURRENTPATH}
+	cp ${CURRENTPATH}/src/mbedtls-${VERSION}/LICENSE ${CURRENTPATH}/include/mbedtls/LICENSE
+	cd ${CURRENTPATH}
+	rm -rf src/mbedtls-${VERSION}
 
 done
-lipo -create ${CURRENTPATH}"/bin/libmbedtls-i386.a" ${CURRENTPATH}"/bin/libmbedtls-x86_64.a" ${CURRENTPATH}"/bin/libmbedtls-armv7.a" ${CURRENTPATH}"/bin/libmbedtls-armv7s.a" ${CURRENTPATH}"/bin/libmbedtls-arm64.a" -output ${CURRENTPATH}"/lib/libmbedtls.a"
+lipo -create ${CURRENTPATH}"/bin/libmbedtls-i386.a" ${CURRENTPATH}"/bin/libmbedtls-armv7.a" ${CURRENTPATH}"/bin/libmbedtls-arm64.a" -output ${CURRENTPATH}"/lib/libmbedtls.a"
 
 echo "Build library..."
 
